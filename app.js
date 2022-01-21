586a1715eab1a3a1579cdc072f3af194ef05ed72
m-ghiringhelli <mghiring@gmail.com>
2022-01-20 16:10:44 -0800

    grabbing DOM elements

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 // import functions
 import { getPokedex } from './fetch.js';
-console.log('app.js is here!!!')
-// grab DOM elements
 
+//checking on import
+//console.log('app.js is here!!!')
+
+// grab DOM elements
+const template = document.querySelector('#template');
+//checking on grab
+//console.log('template', template);
 // set event listeners 
     // get user input
     // use user input to update state 
